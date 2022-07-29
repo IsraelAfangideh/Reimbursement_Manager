3ea8e24635f87d266acc5ed435df2c1ac5eb1880
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2022-07-29 15:43:09 -0500

    Added index to front of directory

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("Mgrbtn").onclick = ManagerLogin
+// document.getElementById("empbtn").onclick = EmployeeLogin
+
+function ManagerLogin () {
+
+    window.location.href = "https://storage.cloud.google.com/carthageayah/ManagerLogin.html"
+}
